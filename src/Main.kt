a421d50d9fbfa4a6f63dd9ed8a00a3e4716b9e34
KamilHeinrich <kamhein51@edu.gdansk.pl>
2024-12-28 17:59:26 +0100

    fizzBuzz

--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,19 @@
 fun main() {
 }
+
+fun fizzBuzz(n: Int): Array<String> {
+    val array = Array(n) {""}
+    var i = 0
+
+    while (i < n) {
+        val temp = i + 1
+        when {
+            temp % 3 == 0 && temp % 5 == 0 -> array[i] = "FizzBuzz"
+            temp % 3 == 0 -> array[i] = "Buzz"
+            temp % 5 == 0 -> array[i] = "Fizz"
+            temp % 3 != 0 && temp % 5 != 0 -> array[i] = temp.toString()
+        }
+        i++
+    }
+    return array
+}
