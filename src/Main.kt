be5750edd41fdf67416f22d0b0efbed2c1c9f50f
KamilHeinrich <kamhein51@edu.gdansk.pl>
2025-01-05 17:57:54 +0100

    All done

---
+++ src/Main.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val student1 = Student("Jan", "Kowalski", intArrayOf(5, 4, 3, 1))
+    val student2 = Student("Kamil", "Heinrich", intArrayOf(2, 6, 5, 4))
+
+    val subject1 = Subject("Algebra", Field.Mathematics, 4)
+    val subject2 = Subject("Thermodynamics", Field.Physics, 2)
+    val subject3 = Subject("Web development", Field.Programming, 3)
+    val subject4 = Subject("Animals", Field.Biology, 1)
+
+    println("${student1.name} ${student1.surname}:")
+    subject1.display()
+    subject2.display()
+
+    println("${student2.name} ${student2.surname}:")
+    subject3.display()
+    subject4.display()
+
+    println("Average grade ${student1.name}: ${student1.avg()}")
+    println("Average grade ${student2.name}: ${student2.avg()}")
+
+    val value = 4.0
+
+    println("${student1.name} has average grade below $value: ${student1.checkAvg(value)}")
+    println("${student2.name} has average grade below $value: ${student2.checkAvg(value)}")
+}
---
+++ src/Subject.kt
@@ -0,0 +1,10 @@
+class Subject(private val name: String, private var field: Field, private val hours: Int) {
+
+    // secondary constructor
+    constructor(name: String, hours: Int) : this(name, Field.Programming, hours)
+
+    // wyswietl dane
+    fun display() {
+        println("Subject $name of field $field has $hours hours.")
+    }
+}
