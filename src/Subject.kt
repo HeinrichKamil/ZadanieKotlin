9643dc43866716b15c9a8fe68d451da1a16e4e0d
HeinrichKamil <hansolo12344321@gmail.com>
2025-02-15 19:49:43 +0100

    FieldOfStudy displays only when provided

--- src/Subject.kt
+++ src/Subject.kt
@@ -1,16 +1,13 @@
 class Subject(private val name: String, private val hours: Int, val grade: Int) {
 
-    private var field: FieldOfStudy
+    private var field: FieldOfStudy? = null
 
     constructor(name: String, field: FieldOfStudy, hours: Int, grade: Int) : this(name, hours, grade) {
         this.field = field
     }
 
-    init {
-        field = FieldOfStudy.Programming // temporary value
-    }
-
     fun display() {
-        println("Subject $name of field $field has $hours hours and grade $grade.")
+        val fieldText = field?.let { "of field $it " } ?: ""
+        println("Subject $name ${fieldText}has $hours hours and grade $grade.")
     }
 }
